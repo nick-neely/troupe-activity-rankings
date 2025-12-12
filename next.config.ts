9ef9e76a974bb693b9b64d951a6a987ab53f7412
nick-neely <neelynickolas@gmail.com>
2025-12-12 08:51:03 -0600

    refactor: clean up configuration files for better readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    nodeMiddleware: true,
-  },
+  // experimental: {
+  //   nodeMiddleware: true,
+  // },
   async rewrites() {
     return [
       {
